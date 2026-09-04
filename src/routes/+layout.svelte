0b92c95dc27973da9b63923e087ce96a2f0eeb7a
Mathias Scherer <mathischerer@gmail.com>
2026-09-04 07:27:39 -0300

    Add mode watcher to project

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,15 @@
 <script lang="ts">
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import { ModeWatcher } from 'mode-watcher';
 
 	let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head>
+	<link rel="icon" href={favicon} />
+	<title>Xera's Blog</title>
+</svelte:head>
+
+<ModeWatcher />
 {@render children()}
